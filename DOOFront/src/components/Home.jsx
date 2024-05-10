b5339f60b6193dfb3f3836bddb5c8d07536fde23
Luis Fernando <luisfgn99@gmail.com>
2024-05-10 01:43:02 -0500

    se crea la pantalla de Home para que muestre saldo y cuenta

--- DOOFront/src/components/Home.jsx
+++ DOOFront/src/components/Home.jsx
@@ -4,9 +4,18 @@ import {getAuth, signOut} from 'firebase/auth';
 const auth = getAuth(appFirebase)
 
 const Home = ({correoUsuario}) =>{
+    const [saldo, setSaldo] = React.useState(0);
+
+    React.useEffect(() => {
+        setSaldo(0);
+    }, [correoUsuario]);
+
     return(
         <div>
-            <h2 className="text-center">Bienvenido usuario {correoUsuario} <button className="btn btn-primary" onClick={()=>signOut(auth)}>Logout</button></h2>
+            <h2 className="text-center">Bienvenido usuario {correoUsuario}</h2>
+            <h3 className="text-center">Número de cuenta: 02485737636</h3>
+            <h3 className="text-center">Saldo: $0.00</h3>
+            <button className="btn btn-primary" onClick={()=>signOut(auth)}>Logout</button>
         </div>
     )
 } 
